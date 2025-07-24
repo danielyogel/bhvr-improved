b34ed77b734b1e3ac9d966878105b95622434610
Daniel Yogel <danielyogel@gmail.com>
2025-07-25 01:33:08 +0300

    better import

--- client/src/lib/orpcClient.ts
+++ client/src/lib/orpcClient.ts
@@ -2,11 +2,11 @@ import type { RouterClient } from '@orpc/server';
 import { createORPCClient, createSafeClient } from '@orpc/client';
 
 import { RPCLink } from '@orpc/client/fetch';
-import type { Router, InferRouterInputs, InferRouterOutputs } from '../../../server/src';
+import type { Router, InferRouterInputs, InferRouterOutputs } from 'server';
 
 const link = new RPCLink({
   url: `${window.location.origin}/rpc`,
-  headers: { Authorization: 'Bearer token' },
+  headers: { Authorization: 'Bearer token' }
 });
 
 const orpc: RouterClient<Router> = createORPCClient(link);
